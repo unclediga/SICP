a267d2e165091c228f14bdda2c7981240c50e7b1
BorodinovI <igordin@yandex.ru>
2014-03-28 16:28:45 +0400

    Упр. 2.44

--- ch2.2.4.rkt
+++ ch2.2.4.rkt
@@ -7,3 +7,30 @@
 ;   (build-path (current-directory)
 ;               "bart.png")))
 ;(paint bart)
+
+;(paint (beside einstein (flip-vert einstein)))
+
+;; -- 2.44------------------------------
+(define (up-split painter n)
+  (if (= n 0)
+  painter
+  (let ((smaller (up-split painter (- n 1))))
+    (below painter (beside smaller smaller)))))
+
+;;-------------------------------------
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
