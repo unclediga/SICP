8e341e3233423b4794da8b6ed9f8c7667c3b2b2b
BorodinovI <igordin@yandex.ru>
2014-10-01 12:22:13 +0400

    section 'Frames'

--- ch2.2.4.rkt
+++ ch2.2.4.rkt
@@ -43,3 +43,48 @@
 (define right-split (split beside below))
 (define up-split (split below beside))
 
+;;-- 2.46 --------------
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect v)
+  (car v))
+
+(define (ycor-vect v)
+  (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+             (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+             (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect v s)
+  (make-vect (* (xcor-vect v) s)
+             (* (ycor-vect v) s)))
+
+;;-- 2.47 --------------
+; variant 1
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame v)
+  (car v))
+(define (edge1-frame v)
+  (cadr v))
+(define (edge2-frame v)
+  (caddr v))
+
+; variant 2
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+; origin-frame  - unchanged
+; edge1-frame   - unchanged
+(define (edge2-frame v)
+  (cddr v))
+
+
+
+
