99a6f6dfa01a26f0c22eac52e2a38ea84e77e82f
BorodinovI <igordin@yandex.ru>
2014-11-24 18:29:37 +0300

    ex2.57

--- ch2.3.2.rkt
+++ ch2.3.2.rkt
@@ -17,17 +17,26 @@
          (error "неизвестный тип выражения -- DERIV" exp))))  
 
 (define (variable? x) (symbol? x))
+
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
 (define (make-sum a1 a2) (list '+ a1 a2))
+
 (define (make-product m1 m2) (list '* m1 m2))
+
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
+
 (define (addend s) (cadr s))
+
 (define (augend s) (caddr s))
+
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
+
 (define (multiplier p) (cadr p))
+
 (define (multiplicand p) (caddr p))
 
 (define (=number? exp num)
@@ -80,3 +89,11 @@
 
 (define (base exp)
   (cadr exp))
+
+;; -- 2.57 ----------------
+
+(define (augend s) 
+  (cond ((pair?(cdddr s)) (cons (car s) (cddr s)))
+        (else
+         (caddr s))))
+
